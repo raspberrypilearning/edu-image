37a219c90982846c1ad3c381b5e8679c02bbbfb8
James Robinson <james.robinson@raspberrypi.org>
2016-03-10 12:39:03 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,27 +5,31 @@ sudo raspi-config nonint set_camera 1
 sudo raspi-config nonint do_i2c 0
 
 sudo sh -c "printf '@lxterminal -e \"/home/pi/edu-install.sh\"' >> .config/lxsession/LXDE-pi/autostart"
-
+sudo sh -c "printf 'hdmi_group=2' >> /boot/config.txt"
+sudo sh -c "printf 'hdmi_mode=86' >> /boot/config.txt"
+read
+echo "Writing config script"
 cat > /home/pi/edu-install.sh << EOL
 set -e
 
 
 echo "Updating"
-sleep 5
+sleep 10
 
 sudo apt-get update
 sudo apt-get -y upgrade 
 sudo apt-get -y dist-upgrade
 sudo rpi-update
-
+read
 echo "Installing from apt"
 sudo apt-get install -y python3-twython tightvncserver python3-smbus vim python-twython python-smbus python-flask python3-flask python-picraft python3-picraft python3-pyqt5 python3-pyqt5.qsci python3-pyqt5.qtserialport gnome-schedule
-
-
+read
+read
 echo "Installing from Pip3"
 sudo pip3 install explorerhat pibrella piglow picraft requests-oauthlib pyinstaller
 sudo pip install explorerhat pibrella piglow picraft requests-oauthlib pyinstaller
 
+read
 mkdir /opt/mu
 cd /opt/mu
 sudo wget https://s3-us-west-2.amazonaws.com/ardublockly-builds/microbit/raspberry_pi/mu-2016-02-16_21_33_00 -O mu
@@ -36,10 +40,12 @@ echo "VNC Setup"
 sed -i '/edu-install/d' /home/pi/.config/lxsession/LXDE-pi/autostart
 tightvncserver
 rm edu-install.sh
-
+echo "ALL DONE!"
 EOL
 
 chmod 755 edu-install.sh
+read
+echo "Configuring Mu & VNC"
 
 cat > mu.desktop << EOL
 
@@ -85,4 +91,6 @@ sudo ln -s /lib/systemd/system/vncserver.service /etc/systemd/system/vncserver.s
 
 sudo systemctl enable vncserver
 
+echo "Ready to Reboot"
+read
 sudo reboot
