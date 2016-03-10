8922490d72e96a5687727e32538aad75f3c29c8e
James Robinson <james.robinson@raspberrypi.org>
2016-03-10 13:28:26 +0000

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -4,6 +4,7 @@ sudo raspi-config nonint do_expand_rootfs
 sudo raspi-config nonint set_camera 1
 sudo raspi-config nonint do_i2c 0
 
+sudo sh -c "printf '@lxterminal -e \"git clone https://github.com/raspberrypilearning/edu-image.git && sudo reboot\"' >> .config/lxsession/LXDE-pi/autostart"
 sudo sh -c "printf '@lxterminal -e \"/home/pi/edu-image/edu-install.sh\"' >> .config/lxsession/LXDE-pi/autostart"
 sudo sh -c "printf 'hdmi_group=2' >> /boot/config.txt"
 sudo sh -c "printf 'hdmi_mode=86' >> /boot/config.txt"
@@ -23,4 +24,6 @@ sudo systemctl enable vncserver
 
 echo "Ready to Reboot"
 read
+sleep 10
 sudo reboot
+
