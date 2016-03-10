ea73bcbd860885ebd687e71087ac1c9eb2d34885
James Robinson <james.robinson@raspberrypi.org>
2016-03-10 19:29:04 +0000

    Update edu-install.sh

--- edu-install.sh
+++ edu-install.sh
@@ -1,23 +1,20 @@
 set -e
-
+sudo sed '/git clone/d' /etc/rc.local
 sleep 5
 echo "Updating"
-
 
 sudo apt-get update
 sudo apt-get -y upgrade 
 sudo apt-get -y dist-upgrade
 sudo rpi-update
-read
+
 echo "Installing from apt"
 sudo apt-get install -y python3-twython tightvncserver python3-smbus vim python-twython python-smbus python-flask python3-flask python-picraft python3-picraft python3-pyqt5 python3-pyqt5.qsci python3-pyqt5.qtserialport gnome-schedule
 
-read
 echo "Installing from Pip3"
 sudo pip3 install explorerhat pibrella piglow picraft requests-oauthlib pyinstaller
 sudo pip install explorerhat pibrella piglow picraft requests-oauthlib pyinstaller
 
-read
 mkdir /opt/mu
 cd /opt/mu
 sudo wget https://s3-us-west-2.amazonaws.com/ardublockly-builds/microbit/raspberry_pi/mu-2016-02-16_21_33_00 -O mu
@@ -40,11 +37,16 @@ sudo mv /home/pi/edu-image/vncserver.service /lib/systemd/system/
 sudo ln -s /lib/systemd/system/vncserver.service /etc/systemd/system/vncserver.service
 sudo systemctl enable vncserver
 
-
 tightvncserver
-rm edu-install.sh
+
+echo "Tidying up"
+sudo sed '/edu-install/d' /etc/rc.local
+
+sudo raspi-config nonint do_wait_for_network Fast
+sudo raspi-config nonint do_boot_behaviour Desktop
+
+rm -rf /home/pi/edu-image
 echo "ALL DONE!"
 echo "Ready to Reboot"
-read
-#
-sudo reboot
+
+#sudo reboot
