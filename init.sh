8d05bec016c44cbc76a07e458ba6bb86a78cf4a6
James Robinson <james.robinson@raspberrypi.org>
2016-03-10 16:20:07 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@
 sudo raspi-config nonint do_expand_rootfs
 sudo raspi-config nonint do_wait_for_network Slow
 sudo raspi-config nonint do_boot_behaviour Console
-sed -i -e '$i \echo "test complete"' /etc/rc.local
+sudo sed -i -e '$i \echo "test complete"' /etc/rc.local
