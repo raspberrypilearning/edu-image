4880684cfe17ced8beeb2db86038b708996c6dd8
James Robinson <james.robinson@raspberrypi.org>
2016-03-10 17:29:40 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,4 +2,6 @@
 sudo raspi-config nonint do_expand_rootfs
 sudo raspi-config nonint do_wait_for_network Slow
 sudo raspi-config nonint do_boot_behaviour Console
-sudo sed -i -e '$i \echo "test complete"' /etc/rc.local
+sudo sed -i -e '$i \cd /home/pi && su pi -c "git clone https://github.com/raspberrypilearning/edu-image.git"' /etc/rc.local
+sudo sed -i -e '$i \cd edu-image && su pi -c "/usr/bin/sh edu-install.sh"' /etc/rc.local
+
