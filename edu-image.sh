4132b24608a8b63ee46c578b0fea12dd4dcf0b7c
Dan <dan@raspberrypi.org>
2017-03-10 09:18:31 +0000

    Update edu-image.sh

--- edu-image.sh
+++ edu-image.sh
@@ -20,6 +20,13 @@ echo "Installing from Pip3"
 sudo pip3 install explorerhat pibrella piglow requests-oauthlib pyinstaller python-sonic pyflakes pep8
 sudo pip install explorerhat pibrella piglow requests-oauthlib pyinstaller 
 
+echo "Installing Mu"
 git clone https://github.com/mu-editor/mu.git
 sudo rm -rf /usr/lib/python3/dist-packages/mu/*
 sudo cp -R ~/mu/mu/* /usr/lib/python3/dist-packages/mu/
+
+echo "Installing Crumble"
+wget http://redfernelectronics.co.uk/?ddownload=3869 -O crumble_0.25.1_all.deb
+sudo apt-get install -y python-numpy python-wxversion libwxbase3.0-0 libwxgtk3.0-0 python-wxgtk3.0 python-pyparsing python-cairo libhidapi-libusb0
+sudo dpkg -i crumble_0.25.1_all.deb 
+rm crumble_0.25.1_all.deb
