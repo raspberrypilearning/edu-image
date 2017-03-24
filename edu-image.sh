b0d4299379c59b46a7e4d53c6fd2ef7176c75bc7
James Robinson <james.robinson@raspberrypi.org>
2017-03-24 10:15:46 +0000

    Update edu-image.sh

--- edu-image.sh
+++ edu-image.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+wget https://github.com/raspberrypilearning/edu-image/blob/master/thonny_0.1_all.deb
+wget https://github.com/raspberrypilearning/edu-image/blob/master/cmdline.txt
+wget https://github.com/raspberrypilearning/edu-image/blob/master/resize2fs_once
+
+read
+
 echo "Raspi-Config steps"
 sudo raspi-config nonint do_camera 0
 sudo raspi-config nonint do_i2c 0
@@ -12,11 +18,15 @@ sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 sudo rpi-update
 
+read
+
 echo "Installing from apt"
 sudo apt-get install -y vim python3-codebug-i2c-tether python3-codebug-tether mu gnome-schedule
 
 echo "Installing from deb"
 sudo dpkg -i thonny_0.1_all.deb
+
+read
 
 echo "Installing from Pip3"
 sudo pip3 install explorerhat pibrella piglow requests-oauthlib pyinstaller python-sonic pyflakes pep8
@@ -27,8 +37,13 @@ git clone https://github.com/mu-editor/mu.git
 sudo rm -rf /usr/lib/python3/dist-packages/mu/*
 sudo cp -R ~/mu/mu/* /usr/lib/python3/dist-packages/mu/
 
+read
+
 echo "Installing Crumble"
 wget http://redfernelectronics.co.uk/?ddownload=3869 -O crumble_0.25.1_all.deb
 sudo apt-get install -y python-numpy python-wxversion libwxbase3.0-0 libwxgtk3.0-0 python-wxgtk3.0 python-pyparsing python-cairo libhidapi-libusb0
 sudo dpkg -i crumble_0.25.1_all.deb 
 rm crumble_0.25.1_all.deb
+
+echo "Complete, ready to halt"
+read
