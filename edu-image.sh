30f522f23c0d37ca53ea34ffe0d0ffd43d0f3cc3
James Robinson <james.robinson@raspberrypi.org>
2017-09-27 08:46:29 +0100

    Update edu-image.sh

--- edu-image.sh
+++ edu-image.sh
@@ -14,18 +14,13 @@ sudo apt-get -qqy dist-upgrade
 sudo rpi-update
 
 echo "Installing from apt"
-sudo apt-get install -qqy mu gnome-schedule
+sudo apt-get install -qqy gnome-schedule
 
 echo "Installing from deb"
 
 echo "Installing from Pip3"
 sudo pip3 -q install explorerhat pibrella piglow requests-oauthlib pyinstaller pyflakes pep8 codebug-i2c-tether codebug-tether
 sudo pip -q install explorerhat pibrella piglow requests-oauthlib pyinstaller 
-
-echo "Installing Mu"
-git clone https://github.com/mu-editor/mu.git
-sudo rm -rf /usr/lib/python3/dist-packages/mu/*
-sudo cp -R ~/mu/mu/* /usr/lib/python3/dist-packages/mu/
 
 echo "Installing Crumble"
 wget -q http://redfernelectronics.co.uk/?ddownload=3869 -O crumble_0.25.1_all.deb
