ce722706857be28b82ccfdd7e41ea0c1b5f12989
James Robinson <james.robinson@raspberrypi.org>
2017-10-16 09:33:52 +0100

    Update edu-image.sh

--- edu-image.sh
+++ edu-image.sh
@@ -33,12 +33,20 @@ wget https://github.com/raspberrypilearning/edu-image/raw/master/Raspbain-Deskto
 sudo mv Raspbain-Desktop-Background-1366x768px.png /usr/share/rpd-wallpaper/picademy.png
 sed -i -e 's/road.jpg/picademy.png/g' .config/pcmanfm/LXDE-pi/desktop-items-0.conf
 
+echo "Installing Mu"
+git clone https://ithub.com/mu-editor/mu.git
+
+sudo apt install python3-pyqt5 python3-pyqt5.qsci python3-pyqt5.qtserialport python3-pyqt5.qtsvg python3-dev
+cd mu
+sudo pip3 install -r requirements.txt
+cd ~/
+
 echo "Setting up Resize"
 sudo wget -q https://raw.githubusercontent.com/raspberrypilearning/edu-image/master/cmdline.txt -O /boot/cmdline.txt
 
 sudo wget -q https://raw.githubusercontent.com/raspberrypilearning/edu-image/master/resize2fs_once -O /etc/init.d/resize2fs_once
 sudo chmod 755 /etc/init.d/resize2fs_once
-sudo ln -s /etc/init.d/resize2fs_once /etc/rc3.d/S01resize2fs_once
+#sudo ln -s /etc/init.d/resize2fs_once /etc/rc3.d/S01resize2fs_once
 
 
 echo "Complete, ready to halt. Type 'sudo halt' and then compress image in another machine."
