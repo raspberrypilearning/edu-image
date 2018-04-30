733df4a4fa1181c96d269053acd18b4ae4c3eff5
James Robinson <james.robinson@raspberrypi.org>
2018-04-30 15:31:14 +0100

    Update edu-image.sh

--- edu-image.sh
+++ edu-image.sh
@@ -15,12 +15,12 @@ sudo apt-get -qqy dist-upgrade
 sudo rpi-update
 
 echo "Installing from apt"
-sudo apt-get install -qqy python-numpy python-wxversion python-wxgtk3.0 python-pyparsing python-cairo libhidapi-libusb0 gnome-schedule python3-pyqt5 python3-pyqt5.qsci python3-pyqt5.qtserialport python3-pyqt5.qtsvg python3-dev libav-tools ffmpeg
+sudo apt-get install -qqy mu python-numpy python-wxversion python-wxgtk3.0 python-pyparsing python-cairo libhidapi-libusb0 gnome-schedule python3-pyqt5 python3-pyqt5.qsci python3-pyqt5.qtserialport python3-pyqt5.qtsvg python3-dev libav-tools ffmpeg
 
 echo "Installing from deb"
 
 echo "Installing from Pip3"
-sudo pip3 -q install python-osc explorerhat pibrella piglow requests-oauthlib pyinstaller codebug-i2c-tether codebug-tether
+sudo pip3 -q install guizero twython python-osc explorerhat pibrella piglow requests-oauthlib pyinstaller codebug-i2c-tether codebug-tether --upgrade
 sudo pip -q install explorerhat pibrella piglow requests-oauthlib pyinstaller 
 
 echo "Installing Crumble"
@@ -32,17 +32,6 @@ echo "Setting Wallpaper"
 wget https://github.com/raspberrypilearning/edu-image/raw/master/Raspbain-Desktop-Background-1366x768px.png
 sudo mv Raspbain-Desktop-Background-1366x768px.png /usr/share/rpd-wallpaper/picademy.png
 sed -i -e 's/road.jpg/picademy.png/g' .config/pcmanfm/LXDE-pi/desktop-items-0.conf
-
-echo "Installing Mu"
-git clone https://github.com/mu-editor/mu.git
-mv mu .mu
-cd .mu
-sudo pip3 install -r requirements_pi.txt
-sudo python3 setup.py install
-sed -i -e 's#Exec=mu#Exec=/home/pi/.mu/run.py#g' conf/mu.desktop
-sudo cp conf/mu.desktop /usr/share/applications/
-sudo cp conf/mu.png /usr/share/pixmaps/
-cd ~/
 
 echo "Setting up Resize"
 sudo wget -q https://raw.githubusercontent.com/raspberrypilearning/edu-image/master/cmdline.txt -O /boot/cmdline.txt
