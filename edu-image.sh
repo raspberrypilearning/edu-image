5cc9156505e441a1ef8ffe747a180ea8dedc6744
James Robinson <james.robinson@raspberrypi.org>
2018-07-26 13:09:04 +0100

    Update edu-image.sh

--- edu-image.sh
+++ edu-image.sh
@@ -15,12 +15,12 @@ sudo apt-get -qqy dist-upgrade
 sudo rpi-update
 
 echo "Installing from apt"
-sudo apt-get install -qqy mu python-numpy python-wxversion python-wxgtk3.0 python-pyparsing python-cairo libhidapi-libusb0 gnome-schedule python3-pyqt5 python3-pyqt5.qsci python3-pyqt5.qtserialport python3-pyqt5.qtsvg python3-dev libav-tools ffmpeg
+sudo apt-get install -qqy mu python-numpy gnome-schedule libav-tools ffmpeg
 
 echo "Installing from deb"
 
 echo "Installing from Pip3"
-sudo pip3 -q install guizero twython python-osc explorerhat pibrella piglow requests-oauthlib pyinstaller codebug-i2c-tether codebug-tether --upgrade
+sudo pip3 -q install pigps guizero twython python-osc explorerhat pibrella piglow requests-oauthlib pyinstaller codebug-i2c-tether codebug-tether --upgrade
 sudo pip -q install explorerhat pibrella piglow requests-oauthlib pyinstaller 
 
 echo "Installing Crumble"
