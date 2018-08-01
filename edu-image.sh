4c676dd63d13ee16ba07be7fa1e5313318f4840d
Marc Scott <marc@raspberrypi.org>
2018-08-01 11:51:22 +0100

    Update edu-image.sh

--- edu-image.sh
+++ edu-image.sh
@@ -32,13 +32,13 @@ rm crumble_0.25.1_all.deb
 echo "Setting Wallpaper"
 wget https://github.com/raspberrypilearning/edu-image/raw/master/Raspbain-Desktop-Background-1366x768px.png
 sudo mv Raspbain-Desktop-Background-1366x768px.png /usr/share/rpd-wallpaper/picademy.png
-sed -i -e 's/road.jpg/picademy.png/g' .config/pcmanfm/LXDE-pi/desktop-items-0.conf
+sed -i -e 's/road.jpg/picademy.png/g' /etc/xdg/pcmanfm/LXDE-pi/desktop-items-0.conf
 
-echo "Setting up Resize"
-sudo wget -q https://raw.githubusercontent.com/raspberrypilearning/edu-image/master/cmdline.txt -O /boot/cmdline.txt
-sudo wget -O /etc/init.d/resize2fs_once https://github.com/RPi-Distro/pi-gen/raw/dev/stage2/01-sys-tweaks/files/resize2fs_once
-sudo chmod +x /etc/init.d/resize2fs_once
-sudo systemctl enable resize2fs_once
+#echo "Setting up Resize"
+#sudo wget -q https://raw.githubusercontent.com/raspberrypilearning/edu-image/master/cmdline.txt -O /boot/cmdline.txt
+#sudo wget -O /etc/init.d/resize2fs_once https://github.com/RPi-Distro/pi-gen/raw/dev/stage2/01-sys-tweaks/files/resize2fs_once
+#sudo chmod +x /etc/init.d/resize2fs_once
+#sudo systemctl enable resize2fs_once
 
 
 echo "Complete, ready to halt. Type 'sudo halt' and then, if cloning, compress image in another machine."
