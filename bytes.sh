d04989dc3b7586945db5f9acbbbb115790d8ef93
James Robinson <james.robinson@raspberrypi.org>
2019-10-29 08:25:19 +0000

    Update bytes.sh

--- bytes.sh
+++ bytes.sh
@@ -9,36 +9,31 @@ echo "AUGUST"
 echo "Updating...."
 sleep 2
 
-#echo 'deb http://plugwash.raspbian.org/sonic-pi buster-sonicpi main ' | sudo tee -a /etc/apt/sources.list
-
-
 sudo apt-get -qq update
 sudo apt-get -qq purge wolfram-engine wolframscript geany scratch scratch2
 sudo apt-get -qqy upgrade 
 sudo apt-get -qqy dist-upgrade
 
 echo "Installing from apt"
-sudo apt install -qqy  scratch3 sonic-pi sonic-pi-server sonic-pi-samples mu-editor ffmpeg
+sudo apt install -qqy  scratch3 sonic-pi sonic-pi-server sonic-pi-samples mu-editor
 echo "Installing from pip3"
-sudo pip3 -q install pigps bluedot guizero twython python-osc explorerhat pibrella piglow requests-oauthlib pyinstaller codebug-i2c-tether codebug-tether --upgrade
-sudo pip -q install explorerhat pibrella piglow requests-oauthlib pyinstaller 
-
-#echo "Installing Crumble"
-#wget -q http://redfernelectronics.co.uk/?ddownload=3869 -O crumble_0.25.1_all.deb
-#sudo apt-get install -qqy python-wxversion libwxbase3.0-0v5 libwxgtk3.0-0v5 python-wxgtk3.0 python-pyparsing libhidapi-libusb0
-#sudo dpkg -i crumble_0.25.1_all.deb 
-#rm crumble_0.25.1_all.deb
+sudo pip3 -q install bluedot guizero twython python-osc explorerhat --upgrade
 
 echo "Setting Wallpaper"
-desktop_background=https://github.com/raspberrypilearning/edu-image/raw/master/Raspbian-Desktop-Background-1366x768px.png
-sudo wget $desktop_background -O /usr/share/rpd-wallpaper/picademy.png
+desktop_background=https://github.com/raspberrypilearning/edu-image/raw/master/Bytes.png
+sudo wget $desktop_background -O /usr/share/rpd-wallpaper/bytes.png
 cp /usr/share/applications/ sonic-pi.desktop /home/pi/Desktop/
 cp /usr/share/applications/ mu.codewith.editor.desktop /home/pi/Desktop/
 cp /usr/share/applications/ scratch3.desktop /home/pi/Desktop/
 mkdir -p /home/pi/.config/pcmanfm/LXDE-pi/
 cp /etc/xdg/pcmanfm/LXDE-pi/desktop-items-0.conf /home/pi/.config/pcmanfm/LXDE-pi/
-sed -i -e 's/temple.jpg/picademy.png/g' /home/pi/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
+sed -i -e 's/temple.jpg/bytes.png/g' /home/pi/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
 sed -i -e 's/mounts=0/mounts=1/g' /home/pi/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
+
+echo "Adding Resources"
+resources=https://github.com/raspberrypilearning/edu-image/raw/master/bytes-resources.zip
+wget $resources -O ~/Desktop/resources.zip
+unzip ~/Desktop/resources.zip
 
 #echo "Setting up Resize"
 sudo wget -q https://raw.githubusercontent.com/raspberrypilearning/edu-image/master/cmdline.txt -O /boot/cmdline.txt
